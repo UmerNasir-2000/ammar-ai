964a82f61ed5df6f3b67b07d7f38d7e661e4f753
Umer Nasir <umer.nasir@razormetrics.com>
2024-08-17 08:32:22 +0500

    feat: add gemini to project

--- app/(dashboard)/dashboard/page.tsx
+++ app/(dashboard)/dashboard/page.tsx
@@ -1,14 +1,31 @@
 'use client';
 
 import { Input } from '@/components/ui/input';
-import { useState } from 'react';
+import { FormEvent, useCallback, useState } from 'react';
 import { TypeAnimation } from 'react-type-animation';
 
+const { GoogleGenerativeAI } = require('@google/generative-ai');
+
+const genAI = new GoogleGenerativeAI(process.env.API_KEY);
+
+const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
+
 export default function Page() {
-  const [inputValue, setInputValue] = useState('');
+  const [prompt, setPrompt] = useState('');
+
+  const onSubmitHandler = useCallback(
+    async (event: FormEvent<HTMLFormElement>) => {
+      event.preventDefault();
+
+      const response = await model.generate({ prompt });
+
+      console.log(response.data);
+    },
+    [prompt]
+  );
 
   return (
-    <div className='flex flex-col h-screen'>
+    <div className='flex flex-col h-screen items-center'>
       <div className='flex-grow p-4 overflow-auto'>
         <TypeAnimation
           sequence={[
@@ -17,23 +34,24 @@ export default function Page() {
             'Two', // Deletes 'One' and types 'Two'
             2000, // Waits 2s
             'Two Three', // Types 'Three' without deleting 'Two'
-            () => {
-              console.log('Sequence completed');
-            },
           ]}
           wrapper='span'
           cursor={true}
-          repeat={Infinity}
           style={{ fontSize: '2em', display: 'inline-block' }}
         />
       </div>
       <div className='w-1/2 fixed bottom-4 left-1/2 transform -translate-x-1/2 p-4'>
-        <div className='w-full bg-black rounded-sm'>
+        <form
+          className='w-full dark:bg-black rounded-md'
+          onSubmit={(e) => onSubmitHandler(e)}
+        >
           <Input
+            value={prompt}
+            onChange={(e) => setPrompt(e.target.value)}
             placeholder='Please ask your query'
             className='w-full shadow-xl p-4 py-6'
           />
-        </div>
+        </form>
       </div>
     </div>
   );
--- components/themeToggle/index.tsx
+++ components/themeToggle/index.tsx
@@ -19,8 +19,8 @@ export default function ModeToggle() {
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
         <Button variant='outline' size='icon'>
-          <IconSunFilled className='h-[1.2rem] w-[1.2rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
-          <IconMoon className='absolute h-[1.2rem] w-[1.2rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
+          <IconSunFilled className='h-[1.5rem] w-[1.5rem] rotate-0 scale-100 transition-all dark:-rotate-90 dark:scale-0' />
+          <IconMoon className='absolute h-[1.5rem] w-[1.5rem] rotate-90 scale-0 transition-all dark:rotate-0 dark:scale-100' />
           <span className='sr-only'>Toggle theme</span>
         </Button>
       </DropdownMenuTrigger>
