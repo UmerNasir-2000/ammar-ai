9954f6b42f078a306ca718ad0714d7ea52dd3aba
Umer Nasir <umer.nasir@razormetrics.com>
2024-08-17 15:13:11 +0500

    feat: use startChat functionality of gemini

--- app/api/prompt/route.ts
+++ app/api/prompt/route.ts
@@ -1,4 +1,3 @@
-import model from '@/lib/gemini';
 import { GoogleGenerativeAI } from '@google/generative-ai';
 import { NextResponse } from 'next/server';
 
@@ -7,11 +6,21 @@ export async function POST(request: Request) {
 
   const genAI = new GoogleGenerativeAI(process.env.API_KEY!);
 
-  const model = genAI.getGenerativeModel({ model: 'gemini-1.5-flash' });
+  const model = genAI.getGenerativeModel({
+    model: 'gemini-1.5-flash',
+    systemInstruction: `Hi, I'm Abbas! I'm your buddy throughout your journey here in conrad labs. I love to help, so feel free to ask me anything you need!`,
+  });
 
-  const { response } = await model.generateContent(query);
+  const response = await model.startChat({});
 
-  const text = response.text();
+  console.log(JSON.stringify(response, null, 2));
 
-  return NextResponse.json({ message: 'Hello, world!', text });
+  const text = await response.sendMessage(query);
+
+  const responseText =
+    text?.response?.candidates?.[0]?.content.parts[0].text ?? '';
+
+  console.log('responseText :>> ', responseText);
+
+  return NextResponse.json({ message: 'Hello, world!', text: responseText });
 }
