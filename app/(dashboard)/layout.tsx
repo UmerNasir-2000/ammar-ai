c739818b78349ed5beec96251d428b50b1ec9605
Umer Nasir <umer.nasir@razormetrics.com>
2024-08-17 07:24:04 +0500

    refactor: move logo components to a separate file

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -1,4 +1,5 @@
 'use client';
+
 import React, { PropsWithChildren, useState } from 'react';
 import {
   IconArrowLeft,
@@ -11,6 +12,7 @@ import { motion } from 'framer-motion';
 import Image from 'next/image';
 import { cn } from '@/lib/utils';
 import { Sidebar, SidebarBody, SidebarLink } from '@/components/ui/sidebar';
+import { Logo, LogoIcon } from '@/components/logo';
 
 const links = [
   {
@@ -86,32 +88,3 @@ export default function DashboardLayout({ children }: PropsWithChildren) {
     </div>
   );
 }
-
-export const Logo = () => {
-  return (
-    <Link
-      href='#'
-      className='font-normal flex space-x-2 items-center text-sm text-black py-1 relative z-20'
-    >
-      <div className='h-5 w-6 bg-black dark:bg-white rounded-br-lg rounded-tr-sm rounded-tl-lg rounded-bl-sm flex-shrink-0' />
-      <motion.span
-        initial={{ opacity: 0 }}
-        animate={{ opacity: 1 }}
-        className='font-medium text-black dark:text-white whitespace-pre'
-      >
-        Ammar AI
-      </motion.span>
-    </Link>
-  );
-};
-
-export const LogoIcon = () => {
-  return (
-    <Link
-      href='#'
-      className='font-normal flex space-x-2 items-center text-sm text-black py-1 relative z-20'
-    >
-      <div className='h-5 w-6 bg-black dark:bg-white rounded-br-lg rounded-tr-sm rounded-tl-lg rounded-bl-sm flex-shrink-0' />
-    </Link>
-  );
-};
